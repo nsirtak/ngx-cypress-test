cbcd23900acae74e6bd4ef8fecf021d982d8ef6a
nsirtak <nsirtak@gmail.com>
2024-07-28 23:36:06 -0500

    second test

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -1,7 +1,9 @@
 /// <reference types="cypress" />
 
 describe('First test suite', () =>{
+
     it('first test', () => {
+
         cy.visit('/')
         cy.contains('Forms').click()
         cy.contains('Form Layouts').click()
@@ -28,10 +30,35 @@ describe('First test suite', () =>{
         cy.get('[placeholder="Email"][fullwidth]')
 
         //by tag, attribute, id and class
-        cy.get('input[placeholder="Email"]#inputEmail1')
+        cy.get('input[placeholder="Email"]#inputEmail1.input-full-width')
 
         //by cypress test ID
         cy.get('[data-cy="imputEmail1"]')
 
     })
+    it.only('second test', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        //Theory
+        // get() - find elements on the page by locator globally
+        // find() - find child elements by locator
+        // contains() - find HTML text and by text and locator
+
+        cy.contains('Sign in')
+        cy.contains('[status="warning"]', 'Sign in')
+        cy.contains('nb-card', 'Horizontal form').find('button')
+        cy.contains('nb-card', 'Horizontal form').contains('Sign in')
+        cy.contains('nb-card', 'Horizontal form').get('button')
+
+        //cypress chains and DOM
+        cy.get('#inputEmail3')
+            .parents('form')
+            .find('button')
+            .should('contain', 'Sign in')
+            .parents('form')
+            .find('nb-checkbox')
+            .click()
+    }) 
 })
